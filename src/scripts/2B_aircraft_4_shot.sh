f9dc86efd8e6c2bf011d0f18218b690cc7d9656a
Ziyu Liu <113190597+Liuziyu77@users.noreply.github.com>
2025-03-04 07:41:14 +0800

    Update 2B_aircraft_4_shot.sh

--- src/scripts/2B_aircraft_4_shot.sh
+++ src/scripts/2B_aircraft_4_shot.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -x
-
 wandb login
 
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-export GPUS_PER_NODE=8
-export NNODES=1
-export MASTER_PORT=29578
-export CPUS_PER_TASK=32
-export QUOTA=reserved
 
 export DATA_PATH=./share_data/aircraft100_4shot
 export CKPT_PATH=./share_models/Qwen2-VL-2B-Instruct
@@ -17,9 +10,6 @@ export SAVE_PATH=./share_models/Qwen2-VL-2B-Instruct_GRPO_aircraft100_4shot_8gpu
 export DEBUG_MODE="true" # Enable Debug if you want to see the rollout of model during RL
 export LOG_PATH="./debug_log_2b_GRPO_aircraft100_4shot.txt"
 
-
-SRUN_ARGS=${SRUN_ARGS:-""}
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 
 torchrun --nnodes $NNODES --nproc_per_node $GPUS_PER_NODE --node_rank $SLURM_NODEID --master_addr $(scontrol show hostname $SLURM_NODELIST | head -n1) --master_port ${MASTER_PORT} /mnt/petrelfs/liuziyu/R1-Grounding/R1-V/src/open-r1-multimodal/src/open_r1/grpo_classification.py \
     --output_dir ${SAVE_PATH}  \
